dcd6d2806d29cacd59b6b56b1cc14f3adcd6d674
TM <tamaramichaud@hotmail.com>
2019-07-23 12:34:13 +0100

    add: deploy.sh to move classes/configs to tomcat area(s)

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,22 @@
-#!/usr/bin/env bash
+#@IgnoreInspection BashAddShebang
+!#/bin/bash
+#set -x (not recognised by cywin...)
 
+rootPath="/cygdrive/c/Program Files (x86)/Apache Software Foundation/Tomcat 9.0/webapps/ROOT"
 
-#stop tomcat
-# clean .xml, clean classes
-# copy .xml & classes from ide to tomcat dir
-# start tomcat
+if [ ! -d "${rootPath}" ] ; then
+  echo "WOOPS, couldn't find the tomcat root dir: $rootPath"
+  exit 1
+fi
 
+"${rootPath}/../..//bin/shutdown.bat"
 
+cp styles.css "${rootPath}"
+cp web.xml "${rootPath}/WEB-INF/"
+rm "${rootPath}/WEB-INF/classes/"
+mkdir "${rootPath}/WEB-INF/classes/"
+cp target/classes/* "${rootPath}/WEB-INF/classes"
 
+${rootPath}/bin/startup.bat
 
-
-## dummy project!!
-
-## Users -> hashmap String, User (string = id, user = firstname, lastname
-## User
-
-
-
-
-#.... -> debugging:  sout goes to Tomcat-installation/logs/catalina.out
+echo "done"
