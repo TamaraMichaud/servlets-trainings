48e62787d01bad93950d1072975ef527c0904f37
TM <tamaramichaud@hotmail.com>
2019-07-31 17:55:23 +0100

    refactor: re-ordered project structure for clarity and using tomcat plugin (successfully!) for debug

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
- net stop Tomcat9
-xcopy "C:\Users\Rayne\IdeaProjects\servlets-trainings\target\java-servlets-trainings-1.0.war" "%CATALINA_HOME%\webapps\"
-net start Tomcat9
+REM # net stop Tomcat9
+REM # xcopy "C:\Users\Rayne\IdeaProjects\servlets-trainings\target\java-servlets-trainings-1.0.war" "%CATALINA_HOME%\webapps\"
+REM # net start Tomcat9
